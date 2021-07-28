351b0dae2ad9a0a821b854b164e107b932f455f1
Curtis Kettler <curtiskettler@gmail.com>
2021-07-28 15:49:05 -0500

    so much stuff

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,17 @@ console.log('JS');
 $(document).ready(onReady);
 
 function onReady() {
-    console.log('JQ');   
+    console.log('JQ');
+    getQuotes();   
+}
+
+function getQuotes() {
+    $.ajax({
+        method: 'GET',
+        url: '/quotes'
+    })
+        .then(function(response) {
+            console.log(response);
+            $('#quotes').append(`<li></li>`)
+        });
 }
