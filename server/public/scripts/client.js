e8760d7b03d50914e6a24caf81915c5f6e915c30
Curtis Kettler <curtiskettler@gmail.com>
2021-07-29 11:04:57 -0500

    all of the stuff and things

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,17 +4,41 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('JQ');
-    getQuotes();   
-}
+    getQuotes();
+    console.log('submitButton', $('#submitButton'));
+    $('#submitButton').on('click', addQuote);
+};
+
+function addQuote() {
+    console.log('inside add quote');
+    let newQuote = {
+        text: $('#quoteInput').val(),
+        author: $('#authorInput').val()
+    };
+    console.log('new quote is', newQuote);
+    $.ajax({
+        method: 'POST',
+        url: '/quotes',
+        data: newQuote
+    }).then((response) => {
+        console.log('post quotes', response);
+        getQuotes();
+    }).catch((error) => {
+        console.log(error);
+        //message to user about error, can be anything
+        $('body').append('<h2>Failure message</h2>');
+    });
+};
 
 function getQuotes() {
     $.ajax({
         method: 'GET',
         url: '/quotes'
-    }) .then(function(response) {
+    }) .then((response) => {
         console.log(response);
         let quoteList = $('#quotes');
         console.log(quoteList);
+        quoteList.empty();
         for (let quote of response){
             quoteList.append(`
             <li>
@@ -23,4 +47,4 @@ function getQuotes() {
             </li>`);
         }
     });
-}
+};
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+
 const quotes = [
     {
         text: 'Debugging is like being the detective in a crime movie where you are also the murderer',
@@ -16,33 +17,30 @@ const quotes = [
     }
 ];
 
-
 app.use(express.static('./server/public'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/quotes', function(req, res) {
+app.get('/quotes', (req, res) => {
     console.log('ready to send back data');
     res.send(quotes);
 });
 
-app.post('/quotes', function (req, res){
+app.post('/quotes', (req, res) => {
     let newQuote = req.body;
-
     if (!newQuote.author || !newQuote.text) {
         res.status(400).send({
             message: 'missing required field'
         });
         return;
     }
-
     quotes.push(newQuote);
     console.log(quotes);
     res.sendStatus(201);   
-})
+});
 
 const port = 5000;
-app.listen(port, function() {
-    console.log('app is running on localhost:5000'); 
+app.listen(port, () => {
+    console.log('app is running on localhost:', port); 
 });
